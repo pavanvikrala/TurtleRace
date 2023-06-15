322a9b3a45f970bea90510f22c685fee2a463347
Vikrala Pavan Kumar <83536024+pavanvikrala@users.noreply.github.com>
2023-06-15 22:23:04 +0530

    main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import random
 is_race_on = False
 screen= Screen()
 screen.setup(width=500, height=400)
-user_choice = screen.textinput(title = "Guess the turtle", prompt= "Which turtle's gonna win?")
+user_choice = screen.textinput(title = "Guess the winning turtle", prompt= "Which turtle's gonna win?")
 
 colors = ['red', 'orange', 'green', 'yellow', 'blue', 'purple', 'chocolate']
-y_positions = [0,40,80,120,-40,-80, -120]
+y_positions = [0,40,80,120,-40,-80,-120]
 turtles = []
 
 for i in range(7):
@@ -18,16 +18,16 @@ for i in range(7):
     new_chitti.goto(-230, y_positions[i])
     turtles.append(new_chitti)
 
-if user_choice :
+if user_choice:
     is_race_on = True
 
-while is_race_on :
+while is_race_on:
 
-    for chitti in turtles :
-        if chitti.xcor() > 230 :
+    for chitti in turtles:
+        if chitti.xcor() > 230:
             is_race_on = False
             winning_color = chitti.pencolor()
-            if winning_color == user_choice :
+            if winning_color == user_choice:
                 print(f"You've won! The {winning_color} chitti is the winner")
             else :
                 print(f"You've lost the race and the winner is {winning_color} color chitti")
